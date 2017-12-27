6b93a01fad7787aca033164b92b73b7008b1f1ad
S4249928 <4249928@studenti.unige.it>
2017-12-27 16:59:25 +0100

    test 1

---
+++ TLIB/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLIB
+{
+    public class LIBSTEST
+    {
+        public string aa { get; set; }
+        public LIBSTEST(string aa) {
+            this.aa = aa;
+        }
+    }
+}
